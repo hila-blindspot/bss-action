2e8af72fc074f7711f206a7c643bb15e3731054a
hila-blindspot <hila@blindspot.security>
2022-07-18 01:34:32 +0300

    upgrade bss-action version

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const repoName = core.getInput('repo-name');
-  console.log(`repository ${repoName}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const runId = JSON.stringify(github.context.runId, undefined, 2)
-  const repo = github.context.repo
-  console.log(`runId: ${runId}`);
-  console.log(`repo: ${repo}`);
-  console.log(`github.token: ${github.token}`);
-  console.log(`github.context.token: ${github.context.token}`);
-
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const repoName = core.getInput("repo-name");
+    console.log(`repository ${repoName}!`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const runId = JSON.stringify(github.context.runId, undefined, 2);
+    const { owner, repo } = github.context.repo;
+    console.log(`runId: ${runId}`);
+    console.log(`repo: ${repo}`);
+    console.log(`github.token: ${github}`);
+    console.log(`github.context.token: ${github.context.token}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
